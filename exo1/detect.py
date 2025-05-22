2e760e34d2e71d70373c3245a8f96edc3ec8fe69
LV-Etienne <elopezvilespy@gmail.com>
2025-05-22 16:47:42 +0200

    distance + affichage logger + test si areamax > 1

--- exo1/detect.py
+++ exo1/detect.py
@@ -6,15 +6,18 @@ from cv_bridge import CvBridge, CvBridgeError
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
+from turtlebot3_msgs.msg import SensorState
 
 
 class Detector(Node):
     def __init__(self):
         super().__init__("detector")
 
+        self.distance : np.float32 = 500.0
+
         self.bridge = CvBridge()
         self.pub_detect = self.create_publisher(Image, "detections", 1)
-        self.pub_chat = self.create_publisher(String, "chatter", 1)
+        # self.pub_chat = self.create_publisher(String, "chatter", 1)
 
         # Determine min and max pixel values
 
@@ -26,31 +29,37 @@ class Detector(Node):
         self.red_min = np.array([[0, 100, 10], [140, 100, 10]], dtype=np.uint8)
         self.red_max = np.array([[10, 230, 245], [180, 230, 245]], dtype=np.uint8)
 
-        self.sub = self.create_subscription(Image, "image_rect", self.callback, 1)
+        self.sub_camera = self.create_subscription(Image, "image_rect", self.callback, 1)
+        self.sub_sonar = self.create_subscription(SensorState, "sensor_state", self.callback_sonar, 1)
+
+    def callback_sonar(self, msg: SensorState) :
+        self.distance = msg.sonar
 
     def callback(self, msg: Image):
         """Process the images going on image_rect"""
 
-        # Convert ROS -> OpenCV
-        try:
-            img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        except CvBridgeError as e:
-            self.get_logger().warn(f"ROS->OpenCV {e}")
-            return
+        if self.distance < 600.0 and self.distance > 10.0:
 
-        img_out = self.detect(img)
+            # Convert ROS -> OpenCV
+            try:
+                img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            except CvBridgeError as e:
+                self.get_logger().warn(f"ROS->OpenCV {e}")
+                return
 
-        # Convert OpenCV -> ROS
-        try:
-            format = "bgr8" if img_out.ndim == 3 else "mono8"
-            msg_out = self.bridge.cv2_to_imgmsg(img_out, format)
-        except CvBridgeError as e:
-            self.get_logger().warn(f"ROS->OpenCV {e}")
-            return
+            img_out = self.detect(img, False)
 
-        self.pub_detect.publish(msg_out)
+            # Convert OpenCV -> ROS
+            try:
+                format = "bgr8" if img_out.ndim == 3 else "mono8"
+                msg_out = self.bridge.cv2_to_imgmsg(img_out, format)
+            except CvBridgeError as e:
+                self.get_logger().warn(f"ROS->OpenCV {e}")
+                return
 
-    def detect(self, img: np.ndarray) -> np.ndarray:
+            self.pub_detect.publish(msg_out)
+
+    def detect(self, img: np.ndarray, direction_fleche_sujet: bool = True) -> np.ndarray:
         # Filter pixels based on their value
         #if HSV image
         img = cv2.cvtColor(img, 40)
@@ -91,17 +100,25 @@ class Detector(Node):
                 areamax = areamax_red
                 imax = imax_red
                 contours = contours_red
-                self.pub_chat.publish(String(data="red"))
+                if direction_fleche_sujet:
+                    self.get_logger().info("Gauche")
+                else:
+                    self.get_logger().info("Droite")
             else:
                 areamax = areamax_blue
                 imax = imax_blue
                 contours = contours_blue
-                self.pub_chat.publish(String(data="blue"))
-            cv2.drawContours(img, contours, imax, (0,255,0), 3,8)
-            # centerx = np.mean(contours[imax][0])
-            # centery = np.mean(contours[imax][1])
-            M = np.mean(contours[imax],axis=0)[0]
-            cv2.circle(img,(int(M[0]),(int(M[1]))),2,(0,255,255),2)
+                if direction_fleche_sujet:
+                    self.get_logger().info("Droite")
+                else:
+                    self.get_logger().info("Gauche")
+
+            if areamax > 1.0:
+                cv2.drawContours(img, contours, imax, (0,255,0), 3,8)
+                # centerx = np.mean(contours[imax][0])
+                # centery = np.mean(contours[imax][1])
+                M = np.mean(contours[imax],axis=0)[0]
+                cv2.circle(img,(int(M[0]),(int(M[1]))),2,(0,255,255),2)
 
         return img
 
